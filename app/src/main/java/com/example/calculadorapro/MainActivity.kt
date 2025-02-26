1188e289620f5c63ff580ed5640aa0e933f2ab78
Nechyuwu <leonardomesta471@gmail.com>
2025-02-26 08:38:42 -0700

    Agregando Diseño sensillo

--- app/src/main/java/com/example/calculadorapro/MainActivity.kt
+++ app/src/main/java/com/example/calculadorapro/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.example.calculadorapro
 
 import android.os.Bundle
+import android.text.style.BackgroundColorSpan
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.calculadorapro.ui.theme.CalculadoraProTheme
 
@@ -19,29 +25,48 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CalculadoraProTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
+                    Calculadora()
+
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Calculadora() {
+    Column (modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center){
+        Text(text = "Aqui van numero")
+
+        Column (modifier = Modifier.fillMaxSize().background(Color.Red) ){
+            Row (horizontalArrangement = Arrangement){
+
+            }
+        }
+    }
+
+
+
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = "Hello !",
+
     )
 }
+
+@Composable
+fun Boton(etiqueta: String){
+    Text(
+        etiqueta, modifier = Modifier.background(Color.Green)
+            .padding(10.dp)
+    )
+}
+
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     CalculadoraProTheme {
-        Greeting("Android")
+        Calculadora("Android")
     }
 }
